1c298d9d980d4984f6d313463ef0a9cf3d45172d
msimonin <matthieu.simonin@inria.fr>
2015-03-05 10:14:28 +0100

    reindent files.
    M

--- openstackg5k/manifests/profile/nfs/client.pp
+++ openstackg5k/manifests/profile/nfs/client.pp
@@ -1,57 +1,57 @@
 #Openstackg5k NFS module for client nodes
 class openstackg5k::profile::nfs::client {
-        $server_ip = hiera('openstack::controller::address::api')
+  $server_ip = hiera('openstack::controller::address::api')
 
-        package { 'nfs-common':
-                ensure => installed,
-        }
+  package { 'nfs-common':
+    ensure => installed,
+  }
 
-        file { '/var/lib/nova/instances':
-                ensure => directory,
-                mode => 'o+xw',
-        }
+  file { '/var/lib/nova/instances':
+    ensure => directory,
+    mode => 'o+xw',
+  }
 
-	file { '/tmp/instances':
-		ensure => directory,
-	}
+  file { '/tmp/instances':
+    ensure => directory,
+  }
 
-        exec { 'mount_instances':
-                command => "mount $server_ip:/var/lib/nova/instances /var/lib/nova/instances; mount $server_ip:/tmp/instances	/tmp/instances",
-                require => Package['nfs-common'],
-                path => '/bin/',
-        }
+  exec { 'mount_instances':
+    command => "mount $server_ip:/var/lib/nova/instances /var/lib/nova/instances; mount $server_ip:/tmp/instances	/tmp/instances",
+    require => Package['nfs-common'],
+    path => '/bin/',
+  }
 
-	exec { 'nfs-to-fstab':
-		command => "/bin/echo \"$server_ip:/var/lib/nova/instances	/var/lib/nova/instances	nfs	auto	0	0\n$server_ip:/tmp/instances	/tmp/instances	nfs	auto	0	0\" >> /etc/fstab",
-		unless => "/bin/grep nfs /etc/fstab",
-	}
+  exec { 'nfs-to-fstab':
+    command => "/bin/echo \"$server_ip:/var/lib/nova/instances	/var/lib/nova/instances	nfs	auto	0	0\n$server_ip:/tmp/instances	/tmp/instances	nfs	auto	0	0\" >> /etc/fstab",
+    unless => "/bin/grep nfs /etc/fstab",
+  }
 
-        exec { 'libvirtd.conf':
-                command => 'sed --in-place=.old1 "s/^#listen_tls = 0.*$/listen_tls = 0/"  /etc/libvirt/libvirtd.conf',
-                require => Package['nfs-common'],
-                path => '/bin/',
-        }
-        exec { 'libvirtd.conf2':
-                command => 'sed --in-place=.old2 "s/^#listen_tcp = 1.*$/listen_tcp = 1\nauth_tcp = \"none\"\n/" /etc/libvirt/libvirtd.conf',
-                require => Package['nfs-common'],
-                path => '/bin/',
-        }
-        exec { 'libvirt-bin.conf':
-                command => 'sed --in-place=.old "s/exec \/usr\/sbin\/libvirtd $libvirtd_opts/exec \/usr\/sbin\/libvirtd $libvirtd_opts -l/" /etc/init/libvirt-bin.conf',
-                require => Package['nfs-common'],
-                path => '/bin/',
-		}
-        exec { 'libvirt-bin':
-                command => 'sed --in-place=.old "s/libvirtd_opts=\"-d\"/libvirtd_opts=\" -d -l\"/" /etc/default/libvirt-bin',
-                require => Package['nfs-common'],
-                path => '/bin/',
-        }
+  exec { 'libvirtd.conf':
+    command => 'sed --in-place=.old1 "s/^#listen_tls = 0.*$/listen_tls = 0/"  /etc/libvirt/libvirtd.conf',
+    require => Package['nfs-common'],
+    path => '/bin/',
+  }
+  exec { 'libvirtd.conf2':
+    command => 'sed --in-place=.old2 "s/^#listen_tcp = 1.*$/listen_tcp = 1\nauth_tcp = \"none\"\n/" /etc/libvirt/libvirtd.conf',
+    require => Package['nfs-common'],
+    path => '/bin/',
+  }
+  exec { 'libvirt-bin.conf':
+    command => 'sed --in-place=.old "s/exec \/usr\/sbin\/libvirtd $libvirtd_opts/exec \/usr\/sbin\/libvirtd $libvirtd_opts -l/" /etc/init/libvirt-bin.conf',
+    require => Package['nfs-common'],
+    path => '/bin/',
+  }
+  exec { 'libvirt-bin':
+    command => 'sed --in-place=.old "s/libvirtd_opts=\"-d\"/libvirtd_opts=\" -d -l\"/" /etc/default/libvirt-bin',
+    require => Package['nfs-common'],
+    path => '/bin/',
+  }
 
-        exec { 'restart_libvirt':
-                command => 'stop libvirt-bin && start libvirt-bin',
-                path => '/sbin/',
-				require => [Exec['libvirt-bin'], Exec['libvirt-bin'],Exec['libvirtd.conf2'],Exec['libvirtd.conf'],Exec['libvirt-bin.conf']],
-        }
-	
+  exec { 'restart_libvirt':
+    command => 'stop libvirt-bin && start libvirt-bin',
+    path => '/sbin/',
+    require => [Exec['libvirt-bin'], Exec['libvirt-bin'],Exec['libvirtd.conf2'],Exec['libvirtd.conf'],Exec['libvirt-bin.conf']],
+  }
+
 }
 
